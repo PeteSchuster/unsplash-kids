3b27145bd659b26040d7b7e7f7b4fdc734d7dadd
Pete Schuster <pas5027@gmail.com>
2020-09-13 21:48:05 -0400

    update title and add back cname

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://localhost:5000/";
-// export const API_BASE = "https://get-image-from-unsplash.herokuapp.com/";
+// export const API_BASE = "http://localhost:5000/";
+export const API_BASE = "https://get-image-from-unsplash.herokuapp.com/";
